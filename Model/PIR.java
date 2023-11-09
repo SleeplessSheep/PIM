9b7f0a7b86285915a208a0cc14bf377f25233f5e
fire-punch <dicky191797520@gmail.com>
2023-11-09 17:27:29 +0800

    Structure the file Creat event and display function are done

--- Model/PIR.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public abstract class PIR { //PIR stand for Personal Information Record
-    string type; //the type of PIR, it could be Contact, Event, Task, TextNote
-    public abstract String getString(); //return the info of the record as String in certain format
-    public String getType(){//return the type
-        return this.type;
-    } 
-}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PIM
 personal information manager 
+
+Jave version "15.0.1" 2020-10-20
+
+Setup:
+cwd: ../PIM
+javac -d build src/Model/*.java
+javac -cp build -d build src/View/*.java
+javac -cp build -d build src/Controller/*.java
+javac -cp build -d build src/*.java
+
